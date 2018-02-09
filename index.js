779851e8408d343b84d82572539d6efd6276a8ff
Quinn Stratton <qgstratton@gmail.com>
2018-02-08 23:03:03 -0600

    added a module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var dt = require('./datemodule.js');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Welcome to Quinn Stratton's Dynamic Website!");
+    response.writeln("Welcome to Quinn Stratton's Dynamic Website!");
     response.write("The date and time are currently: " + dt.myDateTime());
     response.end();
 
