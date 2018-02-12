06e4d3bcaa43ddf1c57f14ccd823aff446e20c93
Quinn Stratton <qgstratton@gmail.com>
2018-02-12 14:42:45 -0600

    testing

--- otherIndex.js
+++ otherIndex.js
@@ -9,8 +9,7 @@ var server = http.createServer(function(request, response) {
         response.end();
     } else {
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Welcome to Quinn Stratton's Dynamic Website!\n");
-        
+        response.write("Welcome to Joe's Dynamic Website!\n"); 
         response.write("The date and time are currently: " + dt.myDateTime());
         response.end();
     }
